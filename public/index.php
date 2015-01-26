b7eae1796a2f81f3d6a1f211419cff8f4b321d3b
JustSteveKing <steve@stevedesign.co.uk>
2015-01-26 19:41:12 +0000

    Adding more Slim functionality

--- public/index.php
+++ public/index.php
@@ -4,10 +4,38 @@
   /* Create Contant Variables Straight Away */
   $app = new \Slim\Slim();
 
+  $app->util = new \Core\Util();
+
+  $app->view = new \Slim\Views\Twig();
+  $app->view->setTemplatesDirectory("src/views/");
+  
+  $app->configureMode('development', function () use ($app) {
+    $app->hook('slim.before', function () use ($app) {
+      $minifier = new MatthiasMullie\Minify\CSS('css/style.css');
+    }
+    $app->config(array(
+      'debug' => true,
+      'database' => array(
+        'db_host' => 'localhost',
+        'db_port' => '',
+        'db_name' => 'table_name',
+        'db_user' => 'db_user',
+        'db_pass' => 'db_password'
+      )
+    ));
+  }
+  
+  $model = new \Core\Model($app->config('database'));
+
   // MORE TO BE ADDED HERE
 
-  $app->get('/', function () use($app) {
-    // This is the Home Page
+  $app->get('/', function () use ($app, $model) {
+    $app->render('head.twig', array(
+      'title' => 'Home'));
+    $app->render('index.twig', array(
+    	'page' => 'Home'
+    ));
+    $app->render('footer.twig');
   });
 
   $app->run();
