e6c1162d36910c55bbac0e6af6e3ea89d2db2d63
JustSteveKing <steve@stevedesign.co.uk>
2015-01-26 16:55:44 +0000

    Starting Slim

--- public/index.php
+++ public/index.php
@@ -1,7 +1,14 @@
 <?php
+  require '../vendor/autoload.php';
 
-require '../vendor/autoload.php';
+  /* Create Contant Variables Straight Away */
+  $app = new \Slim\Slim();
 
+  // MORE TO BE ADDED HERE
 
+  $app->get('/', function () use($app) {
+    // This is the Home Page
+  });
 
+  $app->run();
 ?>
